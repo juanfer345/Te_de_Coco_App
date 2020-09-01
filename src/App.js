179c4922548cbff2b1291c12ea9b88e4a4f631bc
Juan <juadmejiaard@unal.edu.co>
2020-09-01 17:57:25 -0500

    Refactor componente App, reestructura proyecto
    Se agrega package.json al projecto, de forma que ahora puede ser
    inicializado con npm install. Se reestructura App en componente y
    funcion util que parsea el XML

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {UploadFile} from "./Logic/UploadFile";
+
+export const App = () => {
+  return (
+    <div>
+      <UploadFile />
+    </div>
+  );
+
+}
+
+export default App; 
